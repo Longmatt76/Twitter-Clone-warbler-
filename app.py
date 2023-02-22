1803205e79f65c485a569e5eabea34056e760f05
Matthew Long <longm7676@gmail.com>
2023-02-22 15:38:08 -0600

    completed part one

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -210,11 +210,88 @@ def stop_following(follow_id):
     return redirect(f"/users/{g.user.id}/following")
 
 
+
+@app.route('/users/add_like/<int:like_id>', methods=['POST'])
+def add_like(like_id):
+    """add a like for the currently logged in user"""
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    liked_warble = Message.query.get_or_404(like_id)
+    g.user.likes.append(liked_warble)
+    db.session.commit()
+
+    return redirect(f"/users/{g.user.id}/likes")
+
+
+
+@app.route('/users/remove_like/<int:like_id>', methods=['POST'])
+def remove_like(like_id):
+    """Have currently-logged-in-user remove this like"""
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    liked_warble = Message.query.get(like_id)
+    g.user.likes.remove(liked_warble)
+    db.session.commit()
+
+    return redirect(f"/users/{g.user.id}/likes")
+
+
+
+@app.route('/users/<int:user_id>/likes')
+def show_likes(user_id):
+    """ displays a users liked warbles"""
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    user = User.query.get_or_404(user_id)
+    ids = [like.id for like in g.user.likes]
+    messages = (Message
+                .query
+                .filter(Message.user_id.in_(ids))
+                .order_by(Message.timestamp.desc())
+                .limit(100)
+                .all())    
+
+    return render_template('users/likes.html', user=user, messages=messages)
+
+    
+
+
 @app.route('/users/profile', methods=["GET", "POST"])
 def profile():
     """Update profile for current user."""
+    
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
 
-    # IMPLEMENT THIS
+    user= g.user
+    form = UserEditForm(obj=user)
+
+    if form.validate_on_submit():
+        if User.authenticate(user.username,form.password.data): 
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data or "/static/images/default-pic.png"
+            user.header_image_url = form.header_image_url.data or "/static/images/warbler-hero.jpg"
+            user.bio = form.bio.data
+
+            db.session.commit()
+            return redirect(f"/users/{user.id}")
+        flash("Access unauthorized, please reenter password", "danger")
+        
+
+    return render_template('users/edit.html', form=form, user_id=user.id)
+    
+        
 
 
 @app.route('/users/delete', methods=["POST"])
@@ -295,8 +372,10 @@ def homepage():
     """
 
     if g.user:
+        ids = [following.id for following in g.user.following] + [g.user.id]
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(ids))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
