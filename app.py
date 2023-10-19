5c63614d6147fbacd7044070aa4c32f18ab2647d
Matthew Long <longm7676@gmail.com>
2023-10-19 17:24:51 -0500

    made required fixes

--- app.py
+++ app.py
@@ -345,8 +345,13 @@ def messages_destroy(message_id):
         return redirect("/")
 
     msg = Message.query.get(message_id)
-    db.session.delete(msg)
-    db.session.commit()
+    if msg.user_id == g.user.id: 
+        db.session.delete(msg)
+        db.session.commit()
+        flash("Message deleted", "success")
+    else:
+        flash("Access unauthorized", "danger")
+        return redirect("/")
 
     return redirect(f"/users/{g.user.id}")
 
